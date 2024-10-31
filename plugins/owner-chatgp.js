2805ce228456db7708053c2232860e2b44e61a8d
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-10-30 18:26:49 -0600

    Update owner-chatgp.js

--- plugins/owner-chatgp.js
+++ plugins/owner-chatgp.js
@@ -1,5 +1,5 @@
 /* ---------------------------------------------------------------------------------------
-  🍀 • By Richetti y Geridzn
+  🍀 • By Monovan oficial
 -----------------------------------------------------------------------------------------*/
 
 import {randomBytes} from 'crypto';
@@ -14,7 +14,7 @@ const handler = async (m, {conn, text, groupMetadata}) => {
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   const name = await conn.getName(m.sender);
   const groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map((v) => v[0]);
-  const fakegif = {key: {participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? {remoteJid: '6289643739077-1613049930@g.us'} : {})}, message: {'videoMessage': {'title': 'MichiBot', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': 'MichiBot', 'jpegThumbnail': false}}};
+  const fakegif = {key: {participant: `0@s.whatsapp.net`, ...('6289643739077-1613049930@g.us' ? {remoteJid: '6289643739077-1613049930@g.us'} : {})}, message: {'videoMessage': {'title': 'MonkeyBot', 'h': `Hmm`, 'seconds': '99999', 'gifPlayback': 'true', 'caption': 'MonkeyBot', 'jpegThumbnail': false}}};
   const teks = `*MENSAJE OFICIAL MICHIBOT*\n*\n ${text}`;
   for (const id of groups) {
     await conn.sendMessage(id, {text: teks}, {quoted: fakegif});
